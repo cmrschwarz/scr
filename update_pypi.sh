da74ef2261e8ef602bba2a77202a7d7654a9f033
Christian Schwarz <cmrs@cmrs.io>
2022-04-11 12:02:17 +0200

    fix pypi setup

--- update_pypi.sh
+++ update_pypi.sh
@@ -4,5 +4,6 @@ cd "$(dirname "$(readlink -f "$0")")"
 pip install -e .
 rm -rf ./dist/*
 python3 -m build
-./tests/test.py
+./scr/tests/test_cli.py
+./run_pytest.sh
 python3 -m twine upload --verbose --repository pypi dist/*
