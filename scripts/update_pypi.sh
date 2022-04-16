bfb1978cf0501c217941af40dd37d3ffc0e69da0
Christian Schwarz <cmrs@cmrs.io>
2022-04-16 22:29:59 +0200

    use tox -p

--- scripts/update_pypi.sh
+++ scripts/update_pypi.sh
@@ -2,8 +2,9 @@
 set -Eeuo pipefail
 cd "$(dirname "$(readlink -f "$0")")/.."
 pip install -e .
+scr --help >/dev/null # make sure the installed script works
 rm -rf ./dist/*
 python3 -m build
 ./scripts/run_pytest.sh
-tox
+./scripts/run_tox.sh
 python3 -m twine upload --verbose --repository pypi dist/*
